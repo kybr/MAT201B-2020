4985cc316d3fdffb4f4cacd5b6ba3a65b9521ab9
Karl Yerkes <karl.yerkes@gmail.com>
2020-03-12 11:37:38 -0700

    gamma example

--- use-chirplet.cpp
+++ use-chirplet.cpp
@@ -4,22 +4,31 @@
 using namespace al;
 using namespace gam;
 
+const int N = 1000;
 struct MyApp : App {
-  Chirplet<> chirplet;  // a Gaussian enveloped sine
+  Chirplet<> chirplet[N];  // a Gaussian enveloped sine
 
   void onCreate() override {
-    chirplet.length(0.1);
-    chirplet.freq(rnd::uniform(220, 880));
+    for (int i = 0; i < N; i++) {
+      chirplet[i].length(rnd::uniform(0.1, 0.8));
+      chirplet[i].freq(rnd::uniform(220, 880));
+    }
   }
 
   void onSound(AudioIOData& io) override {
     while (io()) {
-      if (chirplet.done()) {
-        chirplet.length(0.1);
-        chirplet.freq(rnd::uniform(220, 880));
+      for (int i = 0; i < N; i++) {
+        if (chirplet[i].done()) {
+          chirplet[i].length(rnd::uniform(0.1, 0.8));
+          chirplet[i].freq(rnd::uniform(220, 880));
+        }
       }
 
-      float s = chirplet().r;
+      float s = 0;
+      for (int i = 0; i < N; i++) {
+        s += chirplet[i]().r;
+      }
+      s /= N;
       io.out(0) = io.out(1) = s;
     }
   }
